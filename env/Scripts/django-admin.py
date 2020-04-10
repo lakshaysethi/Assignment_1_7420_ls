f13cfd1cc28c5b26a05db9106172f2ec5f1fb0ca
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-10 13:58:47 +1200

    del venv

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\kl\ideaprojects\simpleprojects\assignment-1-7420-ls\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
