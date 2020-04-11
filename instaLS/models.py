7c098c6d83041f637f7e4bebf4c37456ca968eae
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-11 19:19:30 +1200

    2

--- instaLS/models.py
+++ instaLS/models.py
@@ -1,3 +1,34 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+from django.contrib.auth.models import User
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    
+
+
+
+class Profile(models.Model):
+    followers = models.CharField(max_length=200)
+    following = models.CharField(max_length=200)
+    
+    
+
+
+# REF
+# from django.db import models
+
+
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
