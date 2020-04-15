96ceddfabce7883aa12c100e98bf9067aa4f4698
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-16 08:43:44 +1200

    Update urls.py

--- instaLS/urls.py
+++ instaLS/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.start, name='start'),
-    path('login/', views.login, name='login'),
+    path('login/', views.loginUser, name='login'),
 path('forgot/', views.forgot, name='forgot'),
+path('profile/', views.profile, name='profile'),
+path('addpost/', views.addPost, name='addPost'),
+path('explore/', views.explore, name='explore'),
+path('home/', views.home, name='home')
+
+
 ]
