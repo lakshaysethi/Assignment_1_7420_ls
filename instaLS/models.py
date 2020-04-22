711b9d12585b500ed8615067ba89b8bd8ebd3a9c
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-22 19:54:57 +1200

    added like model

--- instaLS/models.py
+++ instaLS/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+class Like(models.Model):
+    user= models.ForeignKey(User,on_delete=models.PROTECT)
+    emoji = models.IntegerField()
+
+    def __str__(self):
+        return f'by {self.user.username} emojiid: {self.emoji}'
 
 class Profile(models.Model):
     user= models.OneToOneField(User,on_delete=models.CASCADE)
@@ -20,9 +26,8 @@ class Post(models.Model):
     body = models.CharField(max_length=500)
     by = models.ForeignKey(Profile, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default=timezone.now)
-
+    likes = models.ManyToManyField(Like,blank=True)
 
     def __str__(self):
         return f'{str(self.title)} post'
-
 
