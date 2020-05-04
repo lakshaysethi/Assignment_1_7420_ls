ea319c0599d356e1ecf3efa08d1c1b852f8a9036
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-05-04 13:39:09 +1200

    Update email_test.py

--- instaLS/email_test.py
+++ instaLS/email_test.py
@@ -1,23 +1,29 @@
 import os
 import smtplib
-
+import imghdr
+from email.message import EmailMessage
 
 EMAIL_ADDRESS = 'pythondjangolakshay@gmail.com'
-# os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = 'testing321'
-# os.environ.get('EMAIL_PASS')
+contacts = ['lakshaynew@gmail.com', 'jiyoon90222@gmail.com']
+
+msg = EmailMessage()
+msg['Subject'] = 'This is a Python test email'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = contacts
+
+msg.set_content('This is a plain text email')
+
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="color:SlateGray;">This is an HTML Email!</h1>
+    </body>
+</html>
+""", subtype='html')
 
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-
-    subject = 'test email from python'
-    body = 'test email BODY from python'
-
-    msg = f'{subject}\n\n{body}'
-
-    
-    smtp.sendmail(EMAIL_ADDRESS,'lakshaynew@gmail.com',msg)
+    smtp.send_message(msg)
