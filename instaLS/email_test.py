9763031d4ba8403e25e63700b0b9c60ec1e1893b
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-05-04 13:26:36 +1200

    Create email_test.py

---
+++ instaLS/email_test.py
@@ -0,0 +1,23 @@
+import os
+import smtplib
+
+
+EMAIL_ADDRESS = 'pythondjangolakshay@gmail.com'
+# os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = 'testing321'
+# os.environ.get('EMAIL_PASS')
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    subject = 'test email from python'
+    body = 'test email BODY from python'
+
+    msg = f'{subject}\n\n{body}'
+
+    
+    smtp.sendmail(EMAIL_ADDRESS,'lakshaynew@gmail.com',msg)
