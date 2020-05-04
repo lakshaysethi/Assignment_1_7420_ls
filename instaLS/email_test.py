00bfdf83335338876453995ea5e71a3bef695041
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-05-04 14:01:21 +1200

    Update email_test.py

--- instaLS/email_test.py
+++ instaLS/email_test.py
@@ -1,21 +1,21 @@
 import os
 import smtplib
-
+from email.message import EmailMessage
 
 EMAIL_ADDRESS = 'pythondjangolakshay@gmail.com'
 # os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = 'testing321'
 # os.environ.get('EMAIL_PASS')
 
-
+contacts = ['lakshaynew@gmail.com',]
+msg = EmailMessage()
+msg['From'] = 'lakshaynew@gmail.com'
+msg['To'] = EMAIL_ADDRESS
+msg['BCC'] = contacts
+msg['Subject'] = 'Test Email from python code'
+msg.set_content('hello! how are you ? this message was sent using the python code')
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
-    subject = 'test email from python'
-    body = 'test email BODY from python'
-
-    msg = f'Subject: {subject}\n\n{body}'
-
-    
-    smtp.sendmail(EMAIL_ADDRESS,'lakshaynew@gmail.com',msg)
+    smtp.send_message(msg)
