37006518847fac3a1c2a5b4e8bc74e6927840f23
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-05-06 00:17:59 +1200

    added assignment draft

--- instaLS/models.py
+++ instaLS/models.py
@@ -18,8 +18,6 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username}\'s Profile'
 
-
-
 class Post(models.Model):
     title= models.CharField(max_length=250)
     image = models.ImageField( null=True,blank=True,upload_to='posts')
