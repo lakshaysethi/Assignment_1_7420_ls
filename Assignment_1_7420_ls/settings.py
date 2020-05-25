e55f1f8a5f32c53fce9aa8d905beff33aa08b10d
Lakshay Sethi <45325278+lakshaysethi@users.noreply.github.com>
2020-05-26 04:44:18 +1200

    Update settings.py

--- Assignment_1_7420_ls/settings.py
+++ Assignment_1_7420_ls/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
