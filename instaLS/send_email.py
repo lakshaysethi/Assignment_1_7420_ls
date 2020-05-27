a734b52cb1c1cc7214bcaa0b9d100c533f6a5a95
Lakshay Sethi <45325278+lakshaysethi@users.noreply.github.com>
2020-05-28 02:26:54 +1200

    Update send_email.py

---
+++ instaLS/send_email.py
@@ -0,0 +1,31 @@
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+
+def sendEmailWithSendGrid(customMessage):
+        
+        
+    message = Mail(
+        from_email='lakshay@lakshaysethi.com',
+        to_emails=customMessage["to_emails"],
+        subject=customMessage["subject"],
+        plain_text_content= customMessage["plain_text_content"],
+        html_content=customMessage["html_content"])
+
+
+    
+    
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+        print('sent')
+    except Exception as e:
+        print(e)
+        print(e.body)
