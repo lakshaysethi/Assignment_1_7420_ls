27a505502b615cc40bc6df6ffcb8e863174a0842
Lakshay Sethi <45325278+lakshaysethi@users.noreply.github.com>
2020-05-28 02:48:10 +1200

    Update settings.py

--- Assignment_1_7420_ls/settings.py
+++ Assignment_1_7420_ls/settings.py
@@ -196,4 +196,4 @@ EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-DEFAULT_FROM_EMAIL = 'info@instals.lakshaysethi.com'
+DEFAULT_FROM_EMAIL = 'info@lakshaysethi.com'
