84805d696f1109986df9e1d02aab20685ef59d7a
Lakshay Sethi <45325278+lakshaysethi@users.noreply.github.com>
2020-05-28 22:45:52 +1200

    Update tasks.py for celery settings

--- tasks.py
+++ tasks.py
@@ -1,13 +1,23 @@
 from time import sleep
 import celery
-from django.conf import settings
-app = celery.Celery('instaLS')
+from Assignment_1_7420_ls import settings
 import os
-settings.configure()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Assignment_1_7420_ls.settings')
+ 
+app = celery.Celery('Assignment_1_7420_ls')
+app.config_from_object('django.conf:settings')
+
 app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+ 
+# Load task modules from all registered Django app configs.
+# app.autodiscover_tasks()
+
+
+
 
 @app.task
 def add(x, y):
