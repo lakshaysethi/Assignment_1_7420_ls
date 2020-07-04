37aa05114319e0144cf10a34e658f6abd3f0b473
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-07-04 21:44:21 +1200

    Update app.js

--- frontend-react/src/app.js
+++ frontend-react/src/app.js
@@ -1,15 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import axios from 'axios';
-const App = props => {
-    useEffect(() => {
-      axios.get('/api/profiles')
-        .then(res => setState(res.data))
-    }, [])
-const [state, setState] = useState('')
-return(
-    <div>
-      <p>{state.response_text}</p>
-    </div>
- )
-};
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {} 
+    this.myCoolAfFunction = this.myCoolAfFunction.bind(this)
+  }
+  myCoolAfFunction(){
+      axios.get('/api/profiles').then(result => {
+        this.setState((prevState, props) => ({
+          text: JSON.stringify(result.data)
+      }))
+    })
+
+  
+  }
+  render(){
+    return <div>
+              <button onClick={this.myCoolAfFunction}>get all profile data and display here NOW! abhi chaayeee</button>
+              <div>{this.state.text}</div>
+          </div>
+  }
+}
+
+
+
 export default App;
