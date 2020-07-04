8076477c80545d6d3670a0c01dcb8125844f27f9
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-07-04 20:54:21 +1200

    added basic code

---
+++ frontend-react/src/app.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+const App = props => {
+    useEffect(() => {
+      axios.get('/api/profiles')
+        .then(res => setState(res.data))
+    }, [])
+const [state, setState] = useState('')
+return(
+    <div>
+      <p>{state.response_text}</p>
+    </div>
+ )
+};
+export default App;
