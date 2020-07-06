9fe651df35406f93897f396c679db79a10609dd7
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-07-06 14:49:10 +1200

    after running build command

---
+++ frontend-react/build/precache-manifest.9577bdafd834d17a99b4a4c5b3572dc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db47df9a87bdaaf5bbb132da45595876",
+    "url": "/aws-react/index.html"
+  },
+  {
+    "revision": "c58a31893e85bda908e1",
+    "url": "/aws-react/static/js/2.536489d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aws-react/static/js/2.536489d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d3d421fe526de88bce",
+    "url": "/aws-react/static/js/main.900c8e8c.chunk.js"
+  },
+  {
+    "revision": "d616e235d4932d158c6c",
+    "url": "/aws-react/static/js/runtime-main.5d93167f.js"
+  }
+]);
