c5f60bfce8ab25f5c45b2037cd5956713e3f3c6a
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-07-06 16:12:13 +1200

    Update settings.py

--- Assignment_1_7420_ls/settings.py
+++ Assignment_1_7420_ls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1i*ddp+2$-+ylh)s!g)b^l)*cgan8@yi-_^^5=1j)mp=k@rpjt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['insta-docker-aws.lakshaysethi.com','insta-docker.lakshaysethi.com','instaclone.lakshaysethi.com','instals.herokuapp.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['react-s3-a2.lakshaysethi.com','insta-docker-aws.lakshaysethi.com','insta-docker.lakshaysethi.com','instaclone.lakshaysethi.com','instals.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
